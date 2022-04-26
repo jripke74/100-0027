a180a0769e1201e3dff0fba1d749fc53e923db72
Jeff Ripke <jripke74@gmail.com>
2022-04-26 11:13:03 -0700

    add cart update logic

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -8,7 +8,7 @@ async function addCartItem(req, res, next) {
   let product;
   try {
     product = await Product.findById(req.body.productId);
-  } catch(error) {
+  } catch (error) {
     next(error);
     return;
   }
@@ -24,7 +24,28 @@ async function addCartItem(req, res, next) {
   });
 }
 
+function updateCartItem(req, res) {
+  const cart = res.locals.cart;
+
+  const updatedItemData = cart.updateItem(
+    req.body.productId,
+    req.body.quantity,
+  );
+
+  req.session.cart = cart;
+
+  res.json({
+    message: 'Item updated!',
+    updatedCartData: {
+      newTotalQuantity: cart.totalQuantity,
+      newTotalPrice: cart.totalPrice,
+      updatedItemPrice: updatedItemData.updatedItemPrice,
+    },
+  });
+}
+
 module.exports = {
   addCartItem: addCartItem,
   getCart: getCart,
+  updateCartItem: updateCartItem,
 };
