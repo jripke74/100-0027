487dee65ece87cf9ee78f129ab9ecca319f7a231
Jeff Ripke <jripke74@gmail.com>
2022-04-28 10:59:55 -0700

    multipole changes

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,13 +1,18 @@
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
 
-function getOrders(req, res) {
-  res.render('customer/orders/all-orders');
+async function getOrders(req, res, next) {
+  try {
+    const orders = await Order.findAllForUser(res.locals.uid);
+    res.render('customer/orders/all-orders', {
+      orders: orders,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
-async function addOrder(req, res) {
-  const cart = res.locals.cart;
-
+async function addOrder(req, res, next) {
   let userDocument;
   try {
     userDocument = await User.findById(res.locals.uid);
