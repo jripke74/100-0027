b2783c631956a0261a1fbabdf0895622029caca3
Jeff Ripke <jripke74@gmail.com>
2022-05-05 12:54:40 -0700

    add stripe and logic to make payment

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -9,7 +9,7 @@ async function getOrders(req, res) {
   try {
     const orders = await Order.findAllForUser(res.locals.uid);
     res.render('customer/orders/all-orders', {
-      orders: orders
+      orders: orders,
     });
   } catch (error) {
     next(error);
@@ -37,24 +37,23 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  const session = await stripe.checkout.session.create({
+  const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    line_items: [
-      {
-        // TODO: replace this withe the 'price' of the product you want to sell
+    line_items: cart.items.map(function(item) {
+      return {
         price_data: {
           currency: 'usd',
           product_data: {
-            name: 'Dummy',
+            name: item.product.title,
           },
-          unit_amount_decimal: 10.99,
+          unit_amount: +item.product.price.toFixed(2) * 100,
         },
-        quantity: 1,
-      },
-    ],
+        quantity: item.quantity,
+      }
+    }),
     mode: 'payment',
     success_url: `http://localhost:3000/orders/success`,
-    cancel_url: `hrrp://localhost:3000/orders/failure`,
+    cancel_url: `http://localhost:3000/orders/failure`,
   });
 
   res.redirect(303, session.url);
